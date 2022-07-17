b1d77ee72bf0f79a7b63fbba6b584f421dd2d7d4
Yacob (Kobi) Cohen-Arazi <kobi.cohenarazi@gmail.com>
2022-07-17 23:30:40 +0300

    catr/src/lib.rs changes
    changes run function and added match, for loop
    
    open function is doing match
    
    clap changed to multiple_values

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,5 +1,7 @@
-use std::error::Error;
 use clap::{App, Arg};
+use std::error::Error;
+use std::fs::File;
+use std::io::{self, BufRead, BufReader};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -12,12 +14,12 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn new(files: &Vec<String>,
-        number_lines: bool,
-        number_nonblank_lines: bool) -> Config {
-        Config{files: files.clone(),
+    pub fn new(files: &Vec<String>, number_lines: bool, number_nonblank_lines: bool) -> Config {
+        Config {
+            files: files.clone(),
             number_lines,
-            number_nonblank_lines}
+            number_nonblank_lines,
+        }
     }
 }
 
@@ -30,13 +32,13 @@ pub fn get_args() -> MyResult<Config> {
                 .short('n')
                 .help("Number lines")
                 .takes_value(false)
-                .conflicts_with("number_nonblank")
+                .conflicts_with("number_nonblank"),
         )
         .arg(
             Arg::with_name("number_nonblank")
                 .short('b')
                 .takes_value(false)
-                .help("Number nonblank lines")
+                .help("Number nonblank lines"),
         )
         .arg(
             Arg::with_name("files")
@@ -44,20 +46,31 @@ pub fn get_args() -> MyResult<Config> {
                 .help("Input file(s) [default: -]")
                 .default_value("-")
                 .allow_invalid_utf8(true)
-                .multiple_values(true)
-                //.required(true)
-                //.min_values(1)
+                .multiple_values(true), //.required(true)
+                                        //.min_values(1)
         )
         .get_matches();
     let number = matches.is_present("number");
     let number_nonblank = matches.is_present("number_nonblank");
     let files = matches.values_of_lossy("files").unwrap();
-    Ok(Config::new(&files,
-        number,
-        number_nonblank))
+    Ok(Config::new(&files, number, number_nonblank))
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    dbg!(&config);
+    for filename in config.files {
+        println!("{}", filename);
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(_) => println!("Opened {}", filename),
+        }
+    }
     Ok(())
 }
