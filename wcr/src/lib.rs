3ea55021a943aa1c2177dc923e1084061b3b6dca
Yacob (Kobi) Cohen-Arazi <kobi.cohenarazi@gmail.com>
2023-04-21 22:01:36 -0700

    before changing clap 4.x from builder to derived
    try using derived instead of builder clap 4.x

--- wcr/src/lib.rs
+++ wcr/src/lib.rs
@@ -1,5 +1,8 @@
 use clap::{Arg, ArgAction, Command};
-use std::error::Error;
+use std::{
+    error::Error,
+    io::{BufRead, BufReader},
+};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
@@ -10,6 +13,14 @@ pub struct Config {
     words: bool,
     bytes: bool,
     chars: bool,
+}
+
+#[derive(Debug, PartialEq)]
+pub struct FileInfo {
+    num_lines: usize,
+    num_words: usize,
+    num_bytes: usize,
+    num_chars: usize,
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -55,16 +66,63 @@ pub fn get_args() -> MyResult<Config> {
         )
         .get_matches();
 
+    let mut l = matches.get_flag("lines");
+    let mut w = matches.get_flag("words");
+    let mut b = matches.get_flag("bytes");
+    let c = matches.get_flag("chars");
+    if [l, w, b, c].iter().all(|v| !v) {
+        l = true;
+        w = true;
+        b = true;
+    }
     Ok(Config {
         files: matches.get_many("files").unwrap().cloned().collect(),
-        lines: matches.get_flag("lines"),
-        words: matches.get_flag("words"),
-        bytes: matches.get_flag("bytes"),
-        chars: matches.get_flag("chars"),
+        lines: l,
+        words: w,
+        bytes: b,
+        chars: c,
     })
 }
 
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(std::io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(std::fs::File::open(filename)?))),
+    }
+}
+
 pub fn run(config: Config) -> MyResult<()> {
+    for filename in &config.files {
+        match open(filename) {
+            Err(err) => eprintln!("{}: {}", filename, err),
+            Ok(_) => println!("Opened {}", filename),
+        }
+    }
     println!("{:#?}", config);
     Ok(())
+}
+
+pub fn count(mut file: impl BufRead) -> MyResult<FileInfo> {
+    let mut num_lines = 0;
+    let mut num_words = 0;
+    let mut num_bytes = 0;
+    let mut num_chars = 0;
+    Ok(FileInfo {
+        num_lines,
+        num_words,
+        num_bytes,
+        num_chars,
+    })
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{count, FileInfo};
+    use std::io::Cursor;
+
+    #[test]
+    fn test_count() {
+        let text = "I don't want the world. I just want your half.\r\n";
+        let info = count(Cursor::new(text));
+    }
 }
