ac28162bde2ac219fc96665f718b5fb5a53ca7e9
Yacob (Kobi) Cohen-Arazi <kobi.cohenarazi@gmail.com>
2023-05-07 09:16:40 -0700

    change lib.rs to match expected parsing
    out_file is not Option<String>

--- uniqr/src/lib.rs
+++ uniqr/src/lib.rs
@@ -6,10 +6,10 @@ type MyResult<T> = Result<T, Box<dyn Error>>;
 #[derive(Parser)]
 #[command(author("Yacob (Kobi) Cohen-Arazi <kobi.cohenarazi@gmail.com>"), version("0.1.0"), about("Rust uniq app"), long_about = None)]
 struct Cli {
-    #[arg(value_name = "Output file")]
-    out_file: String,
     #[arg(value_name = "Input file", default_value("-"))]
     in_file: String,
+    #[arg(value_name = "Output file")]
+    out_file: Option<String>,
     #[arg(short('c'), long("count"), help("Show couns"))]
     count: bool,
 }
@@ -24,12 +24,7 @@ pub struct Config {
 
 pub fn get_args() -> MyResult<Config> {
     let cli = Cli::parse();
-    let out = if cli.out_file.is_empty() {
-        None
-    } else {
-        Some(cli.out_file)
-    };
-    Ok(Config {in_file: cli.in_file, out_file: out, count: cli.count})
+    Ok(Config {in_file: cli.in_file, out_file: cli.out_file, count: cli.count})
 }
 
 pub fn run(config: Config) -> MyResult<()> {
