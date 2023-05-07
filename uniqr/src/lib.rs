8ea9fd312ae7fab107a9ffdc41e71b3a6d2f32bf
Yacob (Kobi) Cohen-Arazi <kobi.cohenarazi@gmail.com>
2023-05-07 08:44:53 -0700

    first revision of uniqr
    derive parsing with clap, main, get_args working.

---
+++ uniqr/src/lib.rs
@@ -0,0 +1,38 @@
+use clap::Parser;
+use std::{error::Error};
+
+type MyResult<T> = Result<T, Box<dyn Error>>;
+
+#[derive(Parser)]
+#[command(author("Yacob (Kobi) Cohen-Arazi <kobi.cohenarazi@gmail.com>"), version("0.1.0"), about("Rust uniq app"), long_about = None)]
+struct Cli {
+    #[arg(value_name = "Output file")]
+    out_file: String,
+    #[arg(value_name = "Input file", default_value("-"))]
+    in_file: String,
+    #[arg(short('c'), long("count"), help("Show couns"))]
+    count: bool,
+}
+
+#[derive(Debug)]
+pub struct Config {
+    in_file: String,
+    out_file: Option<String>,
+    count: bool,
+}
+
+
+pub fn get_args() -> MyResult<Config> {
+    let cli = Cli::parse();
+    let out = if cli.out_file.is_empty() {
+        None
+    } else {
+        Some(cli.out_file)
+    };
+    Ok(Config {in_file: cli.in_file, out_file: out, count: cli.count})
+}
+
+pub fn run(config: Config) -> MyResult<()> {
+    println!("{:?}", config);
+    Ok(())
+}
