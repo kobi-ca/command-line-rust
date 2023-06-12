5c675733d815ea82b921b42c9d6acddba1a48b58
Yacob (Kobi) Cohen-Arazi <kobi.cohenarazi@gmail.com>
2023-06-11 18:34:51 -0700

    Fix dies_xyz tests.
    Since we changed clap, the output stderr is different
    expected/string from predicate::std::contains needed to be adjusted

--- findr/tests/cli.rs
+++ findr/tests/cli.rs
@@ -42,14 +42,16 @@ fn dies_bad_name() -> TestResult {
         .args(&["--name", "*.csv"])
         .assert()
         .failure()
-        .stderr(predicate::str::contains("Invalid --name \"*.csv\""));
+        .stderr(predicate::str::contains(
+            "invalid value '*.csv' for '--name [<Name>...]': regex parse error:",
+        ));
     Ok(())
 }
 
 // --------------------------------------------------
 #[test]
 fn dies_bad_type() -> TestResult {
-    let expected = "error: 'x' isn't a valid value for '--type <TYPE>...'";
+    let expected = "invalid value 'x' for '--type [<Entry type>...]'";
     Command::cargo_bin(PRG)?
         .args(&["--type", "x"])
         .assert()
@@ -76,15 +78,13 @@ fn format_file_name(expected_file: &str) -> Cow<str> {
 fn run(args: &[&str], expected_file: &str) -> TestResult {
     let file = format_file_name(expected_file);
     let contents = fs::read_to_string(file.as_ref())?;
-    let mut expected: Vec<&str> =
-        contents.split("\n").filter(|s| !s.is_empty()).collect();
+    let mut expected: Vec<&str> = contents.split("\n").filter(|s| !s.is_empty()).collect();
     expected.sort();
 
     let cmd = Command::cargo_bin(PRG)?.args(args).assert().success();
     let out = cmd.get_output();
     let stdout = String::from_utf8(out.stdout.clone())?;
-    let mut lines: Vec<&str> =
-        stdout.split("\n").filter(|s| !s.is_empty()).collect();
+    let mut lines: Vec<&str> = stdout.split("\n").filter(|s| !s.is_empty()).collect();
     lines.sort();
 
     assert_eq!(lines, expected);
@@ -307,8 +307,7 @@ fn unreadable_dir() -> TestResult {
 
     let out = cmd.get_output();
     let stdout = String::from_utf8(out.stdout.clone())?;
-    let lines: Vec<&str> =
-        stdout.split("\n").filter(|s| !s.is_empty()).collect();
+    let lines: Vec<&str> = stdout.split("\n").filter(|s| !s.is_empty()).collect();
 
     assert_eq!(lines.len(), 17);
 
